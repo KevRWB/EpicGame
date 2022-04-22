07720570435c4ff5a4790e005f84588bab16f5ef
KevRWB <kevinrenaultoff@gmail.com>
2022-04-22 10:04:10 +0200

    creating Monsters and Hero Classes with some instances

--- src/Hero.java
+++ src/Hero.java
@@ -1,11 +1,13 @@
 public class Hero {
 
+    // non static variables
     String name;
     String race;
     int health;
     int initiative;
     int moral;
 
+    //Constructor
     public Hero(String name, String race, int health, int initiative, int moral){
         this.name = name;
         this.race = race;
@@ -15,8 +17,14 @@ public class Hero {
     }
 
     public static void main(String[] args){
-
+        // Creates New Hero(s)
         Hero Kevin = new Hero("Kevin", "Human", 70, 70, 70);
+
+        System.out.println(Kevin);
     }
 
+    //toString Method to print instances values
+    public String toString(){
+        return "This hero is " + this.name +". He's a " + this.race + ". Stats are : Health : " + this.health + ", Initiative : " + this.initiative + ", moral : " + this.moral ;
+    }
 }
--- src/Monster.java
+++ src/Monster.java
@@ -1,11 +1,16 @@
 public class Monster {
+
+    //Non-static Variables for Monster
+    int id;
     String name;
     String race;
     int health;
     int initiative;
     int moral;
 
-    public Monster(String name, String race, int health, int initiative, int moral){
+    //Constructor for Monster
+    public Monster(int id, String name, String race, int health, int initiative, int moral){
+        this.id = id;
         this.name = name;
         this.race = race;
         this.health = health;
@@ -14,9 +19,20 @@ public class Monster {
     }
 
     public static void main(String[] args){
-        Monster orc = new Monster("Green Orc", "Orc", 60, 40, 30 );
-        Monster bat = new Monster("Bat", "Animal", 15, 40, 10 );
-        Monster witch = new Monster("Witch", "Human", 30, 60, 70);
+
+        //Create new Monster(s)
+        Monster orc = new Monster(1,"Green Orc", "Orc", 60, 40, 30 );
+        Monster bat = new Monster(2,"Bat", "Animal", 15, 40, 10 );
+        Monster witch = new Monster(3,"Witch", "Human", 30, 60, 70);
+        //Print values of Monster(s)
+        System.out.println(orc);
+        System.out.println(bat);
+        System.out.println(witch);
+    }
+
+    //toString Method to print instances values
+    public String toString(){
+        return "This monster is " + this.name +" (id : " + this.id +"). He's a " + this.race + ". Stats are : Health : " + this.health + ", Initiative : " + this.initiative + ", moral : " + this.moral ;
     }
 
 }
